367f22466e4582ac3c77c10e2c15d6940205c8e0
Nabil D <nabildroussi@gmail.com>
2017-12-14 16:35:21 +0100

    Fixed connection string issue in ApplicationDbContext.

--- ChainOfRespImpl/Models/ApplicationDbContext.cs
+++ ChainOfRespImpl/Models/ApplicationDbContext.cs
@@ -9,6 +9,16 @@ namespace ChainOfRespImpl.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public ApplicationDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+
+        }
+
         public DbSet<Company> Companies { get; set; }
 
         public DbSet<Employee> Employees { get; set; }
